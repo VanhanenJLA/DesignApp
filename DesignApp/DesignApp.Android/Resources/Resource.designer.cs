3e54986ed9251469ec36e59076dbf5aebffb4e26
Jouni Vanhanen <jouni.vanhanen@outlook.com>
2019-09-17 21:35:46 +0300

    Fix TitleView and icons

--- DesignApp/DesignApp.Android/Resources/Resource.designer.cs
+++ DesignApp/DesignApp.Android/Resources/Resource.designer.cs
@@ -6439,10 +6439,16 @@ namespace DesignApp.Droid
 			public const int notify_panel_notification_icon_bg = 2131165496;
 			
 			// aapt resource value: 0x7F070139
-			public const int tooltip_frame_dark = 2131165497;
+			public const int settings_icon = 2131165497;
 			
 			// aapt resource value: 0x7F07013A
-			public const int tooltip_frame_light = 2131165498;
+			public const int shutter_icon = 2131165498;
+			
+			// aapt resource value: 0x7F07013B
+			public const int tooltip_frame_dark = 2131165499;
+			
+			// aapt resource value: 0x7F07013C
+			public const int tooltip_frame_light = 2131165500;
 			
 			static Drawable()
 			{
