c29d3f01d23ed49e8359625586926f146fcb6210
Jouni Vanhanen <jouni.vanhanen@outlook.com>
2019-09-17 20:37:11 +0300

    Add file management feature

---
+++ DesignApp/DesignApp.Android/DependencyImplementations/FileManagerAndroid.cs
@@ -0,0 +1,41 @@
+﻿using Android.Content;
+using DesignApp.Droid.DependencyImplementations;
+using DesignApp.PlatformDependencies;
+using System;
+using System.IO;
+
+[assembly: Xamarin.Forms.Dependency(typeof(FileManagerAndroid))]
+namespace DesignApp.Droid.DependencyImplementations
+{
+
+    public class FileManagerAndroid : IFileManager
+    {
+        public string GetExternalStoragePublicDirectory()
+        {
+            return Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDcim).AbsolutePath;
+        }
+
+        public byte[] ReadFile(string filePath)
+        {
+            return File.ReadAllBytes(filePath);
+        }
+
+        public void DeleteFile(string filePath)
+        {
+            File.Delete(filePath);
+        }
+
+
+        public void SaveFile(string filePath, byte[] bytes)
+        {
+
+            File.WriteAllBytes(filePath, bytes);
+            var mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
+            mediaScanIntent.SetData(Android.Net.Uri.FromFile(new Java.IO.File(filePath)));
+            Android.App.Application.Context.SendBroadcast(mediaScanIntent);
+
+        }
+
+    }
+
+}
---
+++ DesignApp/DesignApp/PlatformDependencies/FileManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DesignApp.PlatformDependencies
+{
+    public interface IFileManager
+    {
+        byte[] ReadFile(string filePath);
+        void SaveFile(string filename, byte[] bytes);
+        void DeleteFile(string filePath);
+        string GetExternalStoragePublicDirectory();
+    }
+
+    public class FileManager
+    {
+        public static async Task<byte[]> ReadFileAsync(string filePath)
+        {
+            return await Task.Run(() => DependencyService.Get<IFileManager>().ReadFile(filePath));
+        }
+
+        public static async Task SaveFileAsync(string filePath, byte[] bytes)
+        {
+            await Task.Run(() => DependencyService.Get<IFileManager>().SaveFile(filePath, bytes));
+        }
+
+        public static async Task DeleteFileAsync(string filePath)
+        {
+            await Task.Run(() => DependencyService.Get<IFileManager>().DeleteFile(filePath));
+        }
+        public static string GetExternalStoragePublicDirectory()
+        {
+            return DependencyService.Get<IFileManager>().GetExternalStoragePublicDirectory();
+        }
+        public static async Task<string> GetExternalStoragePublicDirectoryAsync()
+        {
+            return await Task.Run(() => DependencyService.Get<IFileManager>().GetExternalStoragePublicDirectory());
+        }
+    }
+}
